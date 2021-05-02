debd1beea23ee8e971f4430911a5be87e40b3f82
lunlun0857 <taso31609@gmail.com>
2021-05-02 16:46:15 +0800

    first commit

---
+++ Planewar/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/************ﬂ[ëÚ≈‰÷√îµì˛************/
+#define GAME_WIDTH  512
+#define GAME_HEIGHT 768
+#define GAME_TITLE  "Plane war v1.0"
+#define GAME_RES_PATH "./plane.rcc"
+#define GAME_ICON ":/new/prefix1/resor/pic.ico"
+#define GAME_RATE 10 //∫¡√Î
+
+
+/************µÿàD≈‰÷√îµì˛************/
+#define MAP_PATH ":/new/prefix1/resor/background.jpg"
+#define MAP_SCROLL_SPEED 2
+
+
+/************ÔwôC≈‰÷√îµì˛************/
+#define MYPLANE_PATH ":/new/prefix1/resor/myplane.png"
+
+#endif // !CONFIG_H
---
+++ Planewar/main.cpp
@@ -0,0 +1,16 @@
+#include "PLANEWAR.h"
+#include <QtWidgets/QApplication>
+#include <qresource.h>
+#include "config.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    
+    //Ô]ƒÔ¶þßMÖÆÙYÔ´
+    QResource::registerResource("GAME_RES_PATH");
+
+    PLANEWAR w;
+    w.show();
+    return a.exec();
+}
